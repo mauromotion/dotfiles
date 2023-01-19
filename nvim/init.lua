93dc8ad2ab1c2bd5d665c6f67d384b1bf922646d
mauromotion <hello@mauromotion.com>
2023-01-19 23:29:40 +0000

    small changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,8 +5,8 @@
 -- * .dotfiles * --
 -- Neovim -- init.lua --------------------------------
 
+require("mm.bootstrap") -- Packer Auto-Installer
 require("mm.base") -- General Settings
 require("mm.highlights") -- Colourscheme and other highlights
 require("mm.maps") -- Keymaps
 require("mm.plugins") -- Plugins
-require("mm.bootstrap") -- Packer Auto-Installer
