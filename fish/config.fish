4b4c2c22187018e193d919d61c3ec976d0c73087
mauromotion <hello@mauromotion.com>
2023-01-21 20:17:07 +0000

    added greeting message to fish

--- fish/config.fish
+++ fish/config.fish
@@ -11,3 +11,14 @@ alias nv="nvim"
 ## rbenv autostart & PATH
 set -x PATH $PATH ~/.rbenv/bin
 rbenv init - | source
+
+## Greeting message
+function ***REMOVED***sh_greeting
+	echo --------------------------------
+	echo ┌┬┐┌─┐┬ ┬┬─┐┌─┐┌┬┐┌─┐┌┬┐┬┌─┐┌┐┌
+	echo │││├─┤│ │├┬┘│ │││││ │ │ ││ ││││
+	echo ┴ ┴┴ ┴└─┘┴└─└─┘┴ ┴└─┘ ┴ ┴└─┘┘└┘
+	echo -------- Fish Shell ------------	
+	echo 
+    echo The time is (set_color yellow; date +%T; set_color normal) and this machine is called $hostname
+end
