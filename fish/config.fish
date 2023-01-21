f639305c62fade41a9c249a78035406ef0bef253
mauromotion <hello@mauromotion.com>
2023-01-21 00:00:06 +0000

    added bashrc

--- fish/config.fish
+++ fish/config.fish
@@ -5,5 +5,9 @@
 ## * .dotfiles * --
 ## Fish terminal -- config.fish --------------
 
+## Aliases
 alias nv="nvim"
 
+## rbenv autostart & PATH
+set -x PATH $PATH ~/.rbenv/bin
+rbenv init - | source
