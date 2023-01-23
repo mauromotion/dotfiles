2304af69da792bc478cbd71cb59f41d8ba676c35
mauromotion <hello@mauromotion.com>
2023-01-23 11:01:31 +0000

    added aliases and nv settings

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,11 @@
 
 ## Aliases
 alias nv="nvim"
+alias vim="nvim"
+alias lg="lazygit"
+alias ..="cd.."
+alias mv="mv -i"
+alias rm="rm -i"
 
 ## rbenv autostart & PATH
 set -x PATH $PATH ~/.rbenv/bin
@@ -23,3 +28,10 @@ function fish_greeting
     echo The time is (set_color yellow; date +%T; set_color normal) and this machine is called $hostname
 	echo
 end
+
+# note: this erases the default fish_mode_prompt, which adds a vi mode
+# indicator to the left of your prompt
+#function fish_mode_prompt; end
+
+# turn on vi mode when the shell starts
+fish_vi_mode
