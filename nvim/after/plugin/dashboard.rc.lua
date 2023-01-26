1b4ee8a6e438e3e7166631795b684f0b5c24f532
mauromotion <hello@mauromotion.com>
2023-01-26 18:56:38 +0000

    upgraded nvim-dashboard config

--- nvim/after/plugin/dashboard.rc.lua
+++ nvim/after/plugin/dashboard.rc.lua
@@ -9,67 +9,53 @@
 local status, db = pcall(require, "dashboard")
 local home = os.getenv("HOME")
 
-db.default_banner = {
-	"",
-	"",
-	" ███▄    █ ▓█████  ▒█████   ██▒   █▓ ██▓ ███▄ ▄███▓",
-	" ██ ▀█   █ ▓█   ▀ ▒██▒  ██▒▓██░   █▒▓██▒▓██▒▀█▀ ██▒",
-	"▓██  ▀█ ██▒▒███   ▒██░  ██▒ ▓██  █▒░▒██▒▓██    ▓██░",
-	"▓██▒  ▐▌██▒▒▓█  ▄ ▒██   ██░  ▒██ █░░░██░▒██    ▒██",
-	"▒██░   ▓██░░▒████▒░ ████▓▒░   ▒▀█░  ░██░▒██▒   ░██▒",
-	"░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒░▒░▒░    ░ ▐░  ░▓  ░ ▒░   ░  ░",
-	"░ ░░   ░ ▒░ ░ ░  ░  ░ ▒ ▒░    ░ ░░   ▒ ░░  ░      ░",
-	"   ░   ░ ░    ░   ░ ░ ░ ▒       ░░   ▒ ░░      ░",
-	"         ░    ░  ░    ░ ░        ░   ░         ░",
-	"                                ░",
-	"",
-	" [ TIP: To exit Neovim, just power off your computer. ] ",
-	"",
-***REMOVED***
--- linux
---db.preview_command = 'ueberzug'
---
---db.preview_***REMOVED***le_path = home .. '/.con***REMOVED***g/nvim/static/neovim.cat'
-db.preview_***REMOVED***le_height = 11
-db.preview_***REMOVED***le_width = 70
-db.custom_center = {
-	{
-		icon = "  ",
-		desc = "Recent sessions                         ",
-		shortcut = "SPC s l",
-		action = "SessionLoad",
-	***REMOVED***,
-	{
-		icon = "  ",
-		desc = "Find recent ***REMOVED***les                       ",
-		action = "Telescope old***REMOVED***les",
-		shortcut = "SPC f r",
-	***REMOVED***,
-	{
-		icon = "  ",
-		desc = "Find ***REMOVED***les                              ",
-		action = "Telescope ***REMOVED***nd_***REMOVED***les ***REMOVED***nd_command=rg,--hidden,--***REMOVED***les",
-		shortcut = "SPC f f",
-	***REMOVED***,
-	{
-		icon = "  ",
-		desc = "File browser                            ",
-		action = "Telescope ***REMOVED***le_browser",
-		shortcut = "SPC f b",
-	***REMOVED***,
-	{
-		icon = "  ",
-		desc = "Find word                               ",
-		action = "Telescope live_grep",
-		shortcut = "SPC f w",
-	***REMOVED***,
-	{
-		icon = "  ",
-		desc = "Load new theme                          ",
-		action = "Telescope colorscheme",
-		shortcut = "SPC h t",
-	***REMOVED***,
-***REMOVED***
-db.custom_footer = { "", "  2023: The year of Linux on Desktop! (WSL)   " ***REMOVED***
-db.session_directory = "/home/mauromotion/.con***REMOVED***g/nvim/session"
-db.session_auto_save_on_exit = true
+db.setup({
+  theme = 'hyper',
+  con***REMOVED***g = {
+    header = {
+    "",
+    "",
+    " ███▄    █ ▓█████  ▒█████   ██▒   █▓ ██▓ ███▄ ▄███▓",
+    " ██ ▀█   █ ▓█   ▀ ▒██▒  ██▒▓██░   █▒▓██▒▓██▒▀█▀ ██▒",
+    "▓██  ▀█ ██▒▒███   ▒██░  ██▒ ▓██  █▒░▒██▒▓██    ▓██░",
+    "▓██▒  ▐▌██▒▒▓█  ▄ ▒██   ██░  ▒██ █░░░██░▒██    ▒██",
+    "▒██░   ▓██░░▒████▒░ ████▓▒░   ▒▀█░  ░██░▒██▒   ░██▒",
+    "░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒░▒░▒░    ░ ▐░  ░▓  ░ ▒░   ░  ░",
+    "░ ░░   ░ ▒░ ░ ░  ░  ░ ▒ ▒░    ░ ░░   ▒ ░░  ░      ░",
+    "   ░   ░ ░    ░   ░ ░ ░ ▒       ░░   ▒ ░░      ░",
+    "         ░    ░  ░    ░ ░        ░   ░         ░",
+    "                                ░",
+    "",
+    " [ TIP: To exit Neovim, just power off your computer. ] ",
+    "",
+    ***REMOVED***,
+  shortcut = {
+        { desc = ' Update', 
+          group = '@property', 
+          action = 'Lazy update', 
+          key = 'u' ***REMOVED***,
+        {
+          desc = ' Files',
+          group = 'Label',
+          action = 'Telescope ***REMOVED***nd_***REMOVED***les',
+          key = 'f',
+        ***REMOVED***,
+        {
+          desc = ' Apps',
+          group = 'DiagnosticHint',
+          action = 'Telescope app',
+          key = 'a',
+        ***REMOVED***,
+        {
+          desc = ' dot***REMOVED***les',
+          group = 'Number',
+          action = 'Telescope dot***REMOVED***les',
+          key = 'd',
+        ***REMOVED***, 
+  ***REMOVED***,
+  packages = { enable = true ***REMOVED***, -- show how many plugins neovim loaded
+  project = { limit = 8, action = 'Telescope ***REMOVED***nd_***REMOVED***les cwd=' ***REMOVED***, -- limit how many projects list, action when you press key or enter it will run this action.
+  mru = { limit = 10 ***REMOVED***, -- how many ***REMOVED***les in list
+  footer = {'', '  2023: The year of Linux on Desktop! (WSL)   ' ***REMOVED***, 
+***REMOVED******REMOVED***)
+
