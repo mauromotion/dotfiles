5a403b3fae1a6bcc2f8f7fb1527aee9f1058d957
mauromotion <hello@mauromotion.com>
2023-01-27 00:48:09 +0000

    added NvimTree

--- nvim/lua/mm/maps.lua
+++ nvim/lua/mm/maps.lua
@@ -11,12 +11,26 @@ local function map(m, k, v)
 	vim.keymap.set(m, k, v, { silent = true ***REMOVED***)
 end
 
--- Keybindings for telescope
-map("n", "<leader>fr", "<CMD>Telescope old***REMOVED***les<CR>")
-map("n", "<leader>ff", "<CMD>Telescope ***REMOVED***nd_***REMOVED***les<CR>")
-map("n", "<leader>fb", "<CMD>Telescope ***REMOVED***le_browser<CR>")
-map("n", "<leader>fw", "<CMD>Telescope live_grep<CR>")
-map("n", "<leader>ht", "<CMD>Telescope colorscheme<CR>")
+-- Load recent sessions
+map('n', '<leader>sl', '<CMD>SessionLoad<CR>')
 
--- Keybindings for NERDTree
-map("n", "<C-b>", "<CMD>NERDTreeToggle<CR>")
+-- Keybindings for telescope
+map('n', '<leader>fr', '<CMD>Telescope old***REMOVED***les<CR>')
+map('n', '<leader>ff', '<CMD>Telescope ***REMOVED***nd_***REMOVED***les<CR>')
+map('n', '<leader>fb', '<CMD>Telescope ***REMOVED***le_browser<CR>')
+map('n', '<leader>fw', '<CMD>Telescope live_grep<CR>')
+map('n', '<leader>ht', '<CMD>Telescope colorscheme<CR>')
+
+-- Keybindings for NvimTree
+map('n', '<C-b>', '<CMD>NvimTreeToggle<CR>')
+
+-- Move to the next/previous buffer
+map('n', '<leader>[', '<CMD>bp<CR>')
+map('n', '<leader>]', '<CMD>bn<CR>')
+
+-- Move to last buffer
+map('n', "''", '<CMD>b#<CR>')
+
+-- Copying the vscode behaviour of making tab splits
+map('n', '<C-\\>', '<CMD>vsplit<CR>')
+map('n', '<A-\\>', '<CMD>split<CR>')
--- nvim/lua/mm/plugins.lua
+++ nvim/lua/mm/plugins.lua
@@ -7,8 +7,8 @@
 
 local status, packer = pcall(require, 'packer')
 if not status then
-	print('Packer is not installed')
-	return
+  print('Packer is not installed')
+  return
 end
 
 -- Reloads Neovim after whenever you save plugins.lua
@@ -20,95 +20,105 @@ augroup END
 ]])
 
 packer.startup(function(use)
-	-- Packer can manage itself
-	use('wbthomason/packer.nvim')
+  -- Packer can manage itself
+  use('wbthomason/packer.nvim')
 
-	-- Dashboard is a nice start screen for nvim
-	use('glepnir/dashboard-nvim')
+  -- Dashboard is a nice start screen for nvim
+  use('glepnir/dashboard-nvim')
 
-	-- Telescope
-	use({
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.0',
-		requires = { { 'nvim-lua/plenary.nvim' ***REMOVED*** ***REMOVED***,
-	***REMOVED***)
-	use('nvim-telescope/telescope-***REMOVED***le-browser.nvim')
-	
-	-- Treesitter Syntax Highlighting
-	use({
-            {
-                'nvim-treesitter/nvim-treesitter',
-                event = 'CursorHold',
-                run = ':TSUpdate',
-                con***REMOVED***g = function()
-                    require('mm.plugins.treesitter')
-                end,
-            ***REMOVED***,
-            { 'nvim-treesitter/playground', after = 'nvim-treesitter' ***REMOVED***,
-            { 'nvim-treesitter/nvim-treesitter-textobjects', after = 'nvim-treesitter' ***REMOVED***,
-            { 'nvim-treesitter/nvim-treesitter-refactor', after = 'nvim-treesitter' ***REMOVED***,
-            { 'windwp/nvim-ts-autotag', after = 'nvim-treesitter' ***REMOVED***,
-            { 'JoosepAlviste/nvim-ts-context-commentstring', after = 'nvim-treesitter' ***REMOVED***,
-        ***REMOVED***)
-	-- Colorschemes
-	use('wittyjudge/gruvbox-material.nvim')
-	use('olimorris/onedarkpro.nvim')
-	use('gruvbox-community/gruvbox')
-	use('martinsione/darkplus.nvim')
-	use('EdenEast/nightfox.nvim')
-	use('shaunsingh/nord.nvim')
-	use{'folke/tokyonight.nvim', branch = main ***REMOVED***
-	use{'catppuccin/nvim', as = 'catppuccin' ***REMOVED***
-	use('rose-pine/neovim')
-	use{'sainnhe/everforest', background = 'hard'***REMOVED***
-	-- use{'neanias/everforest-nvim', branch = main ***REMOVED***
-	
-	-- Autocompletion and Documentation
-	use{'neoclide/coc.nvim', 
-		branch = 'release'***REMOVED***
+  -- Telescope
+  use({
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' ***REMOVED*** ***REMOVED***,
+  ***REMOVED***)
+  use('nvim-telescope/telescope-***REMOVED***le-browser.nvim')
 
-	-- Better editing
-	use('tpope/vim-surround')
-	use('tpope/vim-commentary')
-	use {'windwp/nvim-autopairs',
-		con***REMOVED***g = function() require('nvim-autopairs').setup {***REMOVED*** end***REMOVED***
-	use{'norcalli/nvim-colorizer.lua',
-		con***REMOVED***g = function() require('colorizer').setup {***REMOVED*** end***REMOVED***
+  -- Treesitter Syntax Highlighting
+  use({
+    {
+      'nvim-treesitter/nvim-treesitter',
+      event = 'CursorHold',
+      run = ':TSUpdate',
+      con***REMOVED***g = function()
+        require('mm.plugins.treesitter')
+      end,
+    ***REMOVED***,
+    { 'nvim-treesitter/playground', after = 'nvim-treesitter' ***REMOVED***,
+    { 'nvim-treesitter/nvim-treesitter-textobjects', after = 'nvim-treesitter' ***REMOVED***,
+    { 'nvim-treesitter/nvim-treesitter-refactor', after = 'nvim-treesitter' ***REMOVED***,
+    { 'windwp/nvim-ts-autotag', after = 'nvim-treesitter' ***REMOVED***,
+    { 'JoosepAlviste/nvim-ts-context-commentstring', after = 'nvim-treesitter' ***REMOVED***,
+  ***REMOVED***)
+  -- Colorschemes
+  use('wittyjudge/gruvbox-material.nvim')
+  use('olimorris/onedarkpro.nvim')
+  use('gruvbox-community/gruvbox')
+  use('martinsione/darkplus.nvim')
+  use('EdenEast/nightfox.nvim')
+  use('shaunsingh/nord.nvim')
+  use{'folke/tokyonight.nvim', branch = main ***REMOVED***
+  use{'catppuccin/nvim', as = 'catppuccin' ***REMOVED***
+  use('rose-pine/neovim')
+  use{'sainnhe/everforest', background = 'hard'***REMOVED***
+  -- use{'neanias/everforest-nvim', branch = main ***REMOVED***
 
-	-- Better UI/UX
-	use('ryanoasis/vim-devicons')
-	use('scrooloose/nerdtree')
-  use ('nvim-tree/nvim-web-devicons')
+  -- Autocompletion and Documentation
+  use{'neoclide/coc.nvim', 
+    branch = 'release'***REMOVED***
 
-	-- Status line
-	use({
-		'nvim-lualine/lualine.nvim',
-		con***REMOVED***g = function()
-			require('mm.plugins.lualine')
-		end,
-	***REMOVED***)
+  -- Better editing
+  use('tpope/vim-surround')
+  use('tpope/vim-commentary')
+  use {'windwp/nvim-autopairs',
+    con***REMOVED***g = function() require('nvim-autopairs').setup {***REMOVED*** end***REMOVED***
+  use{'norcalli/nvim-colorizer.lua',
+    con***REMOVED***g = function() require('colorizer').setup {***REMOVED*** end***REMOVED***
 
-	-- Zen modes
-	use({
-		'Pocco81/true-zen.nvim',
-		con***REMOVED***g = function()
-			require('true-zen').setup {
-				-- your con***REMOVED***g goes here
-				-- or just leave it empty :)
-			***REMOVED***
-		end,
-	***REMOVED***)
+  -- Better UI/UX
+  use('ryanoasis/vim-devicons')
+  -- use('scrooloose/nerdtree')
+  -- use ('nvim-tree/nvim-web-devicons')
+  use {
+    'nvim-tree/nvim-tree.lua',
+    con***REMOVED***g = function()
+      require('nvim-tree').setup()
+    end,
 
-	-- Live Server
-	use({
-		'aurum77/live-server.nvim',
-		run = function()
-			require'live_server.util'.install()
-		end,
-		cmd = { 'LiveServer', 'LiveServerStart', 'LiveServerStop' ***REMOVED***,
-	***REMOVED***)
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional, for ***REMOVED***le icons
+    ***REMOVED***,
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  ***REMOVED***
+  -- Status line
+  use({
+    'nvim-lualine/lualine.nvim',
+    con***REMOVED***g = function()
+      require('mm.plugins.lualine')
+    end,
+  ***REMOVED***)
 
-	if packer_bootstrap then
-		packer.sync()
-	end
+  -- Zen modes
+  use({
+    'Pocco81/true-zen.nvim',
+    con***REMOVED***g = function()
+      require('true-zen').setup {
+        -- your con***REMOVED***g goes here
+        -- or just leave it empty :)
+      ***REMOVED***
+    end,
+  ***REMOVED***)
+
+  -- Live Server
+  use({
+    'aurum77/live-server.nvim',
+    run = function()
+      require'live_server.util'.install()
+    end,
+    cmd = { 'LiveServer', 'LiveServerStart', 'LiveServerStop' ***REMOVED***,
+  ***REMOVED***)
+
+  if packer_bootstrap then
+packer.sync()
+  end
 end)
