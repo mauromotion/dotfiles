7485f7e0f5b49ac0d8976d8e8ea8f1cf225b3537
mauromotion <hello@mauromotion.com>
2023-01-27 00:48:54 +0000

    added telescope config

---
+++ nvim/after/plugin/telescope.rc.lua
@@ -0,0 +1,26 @@
+--
+local status, telescope = pcall(require, "telescope")
+if not status then
+  return
+end
+
+telescope.setup({
+  extensions = {
+***REMOVED***le_browser = {
+      theme = "ivy",
+      -- disables netrw and use telescope-***REMOVED***le-browser in its place
+      hijack_netrw = true,
+      mappings = {
+        ["i"] = {
+          -- your custom insert mode mappings
+        ***REMOVED***,
+        ["n"] = {
+          -- your custom normal mode mappings
+        ***REMOVED***,
+      ***REMOVED***,
+    ***REMOVED***,
+  ***REMOVED***,
+***REMOVED***)
+
+telescope.load_extension("***REMOVED***le_browser")
+
