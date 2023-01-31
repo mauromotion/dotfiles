93d4fb0722d84a2c91dc5b60032bc94592f1ee9c
mauromotion <hello@mauromotion.com>
2023-01-31 17:32:06 +0000

    moved onedark config to its own lua file

---
+++ nvim/lua/mm/plugins/onedarkpro.lua
@@ -0,0 +1,22 @@
+require('onedarkpro').setup({
+  options = {
+    cursorline = true,
+    highlight_inactive_windows = true
+  },
+  styles = {
+    types = "NONE",
+    methods = "NONE",
+    numbers = "NONE",
+    strings = "NONE",
+    comments = "italic",
+    keywords = "bold,italic",
+    constants = "NONE",
+    functions = "italic",
+    operators = "NONE",
+    variables = "NONE",
+    parameters = "NONE",
+    conditionals = "italic",
+    virtual_text = "NONE",
+  }
+ 
+})
