ecffcede972c48fe2f697197f653ed12a1578326
mauromotion <hello@mauromotion.com>
2023-01-31 00:54:28 +0000

    switched main theme to onedark

--- nvim/lua/mm/highlights.lua
+++ nvim/lua/mm/highlights.lua
@@ -10,7 +10,8 @@
 -- Uncomment just ONE of the following colorschemes!
 -- local ok, _ = pcall(vim.cmd, 'colorscheme onedark')
 -- local ok, _ = pcall(vim.cmd, "colorscheme gruvbox-material")
-local ok, _ = pcall(vim.cmd, "colorscheme everforest")
+-- local ok, _ = pcall(vim.cmd, "colorscheme everforest")
+local ok, _ = pcall(vim.cmd, "colorscheme onedark")
 
 -- Highlight the region on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
