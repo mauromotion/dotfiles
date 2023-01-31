37046580d11deda2750b0f2fa3e32b8b4a0c50ee
mauromotion <hello@mauromotion.com>
2023-01-31 14:40:54 +0000

    added nvim key bindings

--- nvim/lua/mm/maps.lua
+++ nvim/lua/mm/maps.lua
@@ -24,13 +24,32 @@ map('n', '<leader>ht', '<CMD>Telescope colorscheme<CR>')
 -- Keybindings for NvimTree
 map('n', '<C-b>', '<CMD>NvimTreeToggle<CR>')
 
--- Move to the next/previous buffer
-map('n', '<leader>[', '<CMD>bp<CR>')
-map('n', '<leader>]', '<CMD>bn<CR>')
-
--- Move to last buffer
-map('n', "''", '<CMD>b#<CR>')
-
 -- Copying the vscode behaviour of making tab splits
 map('n', '<C-\\>', '<CMD>vsplit<CR>')
 map('n', '<A-\\>', '<CMD>split<CR>')
+
+-- Move current line / block with Alt-j/k ala vscode (Insert mode).
+map('i', '<A-j>', '<Esc>:m .+1<CR>==gi')
+map('i','<A-k>', '<Esc>:m .-2<CR>==gi')
+
+-- Move current line / block with Alt-j/k ala vscode (Visual mode).
+map('v', '<A-j>', ":m '>+1<CR>gv-gv")
+map('v', '<A-k>', ":m '<-2<CR>gv-gv")
+
+-- Navigation
+map('i', '<A-Up>', '<C-\\><C-N><C-w>k')
+map('i', '<A-Down>', '<C-\\><C-N><C-w>j')
+map('i', '<A-Left>', '<C-\\><C-N><C-w>h')
+map('i', '<A-Right>', '<C-\\><C-N><C-w>l')
+
+-- Better window movement
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
+
+-- Resize with arrows
+map('n', '<C-Up>', ':resize -2<CR>')
+map('n', '<C-Down>', ':resize +2<CR>')
+map('n', '<C-Left>', ':vertical resize -2<CR>')
+map('n', '<C-Right>', ':vertical resize +2<CR>')
