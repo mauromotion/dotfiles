00c740becfc2822ff56a4b95cd20f057dddb6114
mauromotion <hello@mauromotion.com>
2023-01-31 17:23:30 +0000

    added configs for onedark theme

--- nvim/lua/mm/plugins.lua
+++ nvim/lua/mm/plugins.lua
@@ -51,30 +51,14 @@ packer.startup(function(use)
     { 'JoosepAlviste/nvim-ts-context-commentstring', after = 'nvim-treesitter' ***REMOVED***,
   ***REMOVED***)
   -- Colorschemes
+  use({
+      'olimorris/onedarkpro.nvim',
+      con***REMOVED***g = function()
+        require('mm.plugins.onedarkpro')
+      end,
+  ***REMOVED***)
   use('wittyjudge/gruvbox-material.nvim')
-  use{'olimorris/onedarkpro.nvim',
-    con***REMOVED***g = function() 
-      require('onedarkpro').setup({
-      options = {
-        cursorline = true,
-        highlight_inactive_windows = true
-      ***REMOVED***,
-      styles = {
-        types = "NONE",
-        methods = "NONE",
-        numbers = "NONE",
-        strings = "NONE",
-        comments = "italic",
-        keywords = "bold,italic",
-        constants = "NONE",
-        functions = "italic",
-        operators = "NONE",
-        variables = "NONE",
-        parameters = "NONE",
-        conditionals = "italic",
-        virtual_text = "NONE",
-      ***REMOVED***
-    ***REMOVED***) end***REMOVED***
+  use('shatur/neovim-ayu')
   use('gruvbox-community/gruvbox')
   use('martinsione/darkplus.nvim')
   use('EdenEast/nightfox.nvim')
