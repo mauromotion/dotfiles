37599dedf66d64937ef48127df1a811e652c2189
mauromotion <hello@mauromotion.com>
2023-02-10 23:12:57 +0000

    awesomeWM - started working on themes

---
+++ awesome/themes/clone/theme.lua
@@ -0,0 +1,22 @@
+
+local awful = require("awful")
+awful.util = require("awful.util")
+
+theme_path = awful.util.getdir("con***REMOVED***g") .. "/themes/clone/"
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+-- default variables
+
+theme = {***REMOVED***
+
+do***REMOVED***le(theme_path .. "elements.lua")
+do***REMOVED***le(theme_path .. "titlebar.lua")
+do***REMOVED***le(theme_path .. "layouts.lua")
+
+theme.wallpaper          = theme_path .. "background.jpg"
+theme.awesome_icon       = theme_path .. "launcher/logo20_kali_black.png"
+theme.awesome_subicon    = theme_path .. "launcher/logo20_kali_black.png"
+
+return theme
+
+
