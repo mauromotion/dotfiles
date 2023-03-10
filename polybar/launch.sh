0ea468c18cca281c20fd854254a556c5a0e5d2d8
mauromotion <hello@mauromotion>
2023-03-10 13:11:55 +0000

    activated polybar on laptop, major conf work

---
+++ polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
+
+# Launch Polybar, using default con***REMOVED***g location ~/.con***REMOVED***g/polybar/con***REMOVED***g.ini
+polybar laptop_Nord 2>&1 | tee -a /tmp/polybar.log & disown
+
+echo "Polybar launched..."
