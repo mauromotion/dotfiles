3db38a89805964dc21e0b8c9a01c492544af7dac
mauromotion <hello@mauromotion.com>
2023-04-10 21:25:56 +0100

    moved to lazyvim

--- nvim.bak/lua/mm/lsp/handlers.lua
+++ nvim.bak/lua/mm/lsp/handlers.lua
@@ -69,18 +69,13 @@ local function lsp_keymaps(bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
   -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gt", require('telescope.builtin').lsp_references, opts) -- it doesn't work
   -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
   -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
-  vim.api.nvim_buf_set_keymap(
-    bufnr,
-    "n",
-    "gl",
-    '<cmd>lua vim.diagnostic.open_float(0, { scope = "line", border = "rounded" })<CR>',
-    opts
-  )
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", '<cmd>lua vim.diagnostic.open_float(0, { scope = "line", border = "rounded" })<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
   vim.cmd [[ command! Format execute 'lua vim.lsp.buf.format()' ]]
 end
 
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,2 @@
-------------------------------------------------------
--- ┌┬┐┌─┐┬ ┬┬─┐┌─┐┌┬┐┌─┐┌┬┐┬┌─┐┌┐┌
--- │││├─┤│ │├┬┘│ │││││ │ │ ││ ││││
--- ┴ ┴┴ ┴└─┘┴└─└─┘┴ ┴└─┘ ┴ ┴└─┘┘└┘
--- * .dotfiles * --
--- Neovim -- init.lua --------------------------------
-
-require("mm.bootstrap") -- Packer Auto-Installer
-require("mm.base") -- General Settings
-require("mm.plugins") -- Plugins
-require("mm.highlights") -- Colorscheme and other highlights
-require("mm.maps") -- Keymaps
-require("mm.cmp") -- Autocompletion
-require("mm.lsp") -- LSP
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
