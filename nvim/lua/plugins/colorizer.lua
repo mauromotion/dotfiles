bad983f3311cd022dc80cc71d3394cf0e07e8a74
mauromotion <hello@mauromotion.com>
2023-04-12 10:45:53 +0100

    added colorizer to lazyvim

---
+++ nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,9 @@
+-- return {
+--   "norcalli/nvim-colorizer.lua",
+--   keys = { { "<leader>co", "<cmd>ColorizerToggle<cr>", desc = "Colorizer" } },
+-- }
+
+return {
+  "brenoprata10/nvim-highlight-colors",
+  keys = { { "<leader>co", "<cmd>HighlightColorsToggle<cr>", desc = "Colorizer" } },
+}
