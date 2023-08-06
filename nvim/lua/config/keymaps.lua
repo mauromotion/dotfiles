5f6d643c00621d416cc3beb33c4b450453b293a3
mauromotion <hello@mauromotion.com>
2023-08-06 17:48:33 +0100

    added harpoon plugin for neovim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<C-h>", function()
+  ui.nav_file(1)
+end)
+vim.keymap.set("n", "<C-j>", function()
+  ui.nav_file(2)
+end)
+vim.keymap.set("n", "<C-k>", function()
+  ui.nav_file(3)
+end)
+vim.keymap.set("n", "<C-l>", function()
+  ui.nav_file(4)
+end)
