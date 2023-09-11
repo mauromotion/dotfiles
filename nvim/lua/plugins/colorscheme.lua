a17bf03548a319984ec2aeade3d7fb74d3a65279
mauromotion <hello@mauromotion.com>
2023-09-12 00:00:40 +0100

    removed transparency

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -20,7 +20,7 @@ return {
     config = function()
       vim.g.gruvbox_material_foreground = "material"
       vim.g.gruvbox_material_background = "medium"
-      vim.g.gruvbox_material_transparent_background = 1
+      vim.g.gruvbox_material_transparent_background = 0
       vim.cmd.colorscheme("gruvbox-material")
     end,
   },
