25f9c53264c1f9a102034ad0654459f711974ccf
mauromotion <hello@mauromotion.com>
2023-09-21 11:34:20 +0100

    split color schemes into individual files

--- nvim_mauro/lua/plugins/UI/gruvbox-material.lua
+++ nvim_mauro/lua/plugins/UI/gruvbox-material.lua
@@ -6,19 +6,4 @@ return {
 		vim.g.gruvbox_material_background = "medium"
 		vim.g.gruvbox_material_transparent_background = 0
 	end,
-
-	{
-		"edeneast/nightfox.nvim",
-    lazy = true,
-		opts = {
-			options = {
-				transparent = true,
-				styles = {
-					comments = "italic",
-					keywords = "bold",
-					types = "italic,bold",
-				},
-			},
-		},
-	},
 }
---
+++ nvim_mauro/lua/plugins/UI/nightfox.lua
@@ -0,0 +1,14 @@
+return {
+	"edeneast/nightfox.nvim",
+	-- lazy = true,
+	opts = {
+		options = {
+			transparent = false,
+			styles = {
+				comments = "italic",
+				keywords = "bold",
+				types = "italic,bold",
+			},
+		},
+	},
+}
