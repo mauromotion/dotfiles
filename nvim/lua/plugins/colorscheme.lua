43faab02348892e95461cf31a2d0562327a6fcd8
mauromotion <hello@mauromotion.com>
2023-09-21 23:03:59 +0100

    switched to everforest theme on desktop

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -18,7 +18,7 @@ return {
     config = function()
       require("everforest").setup({
         background = "hard",
-        transparent_background_level = 0,
+        transparent_background_level = 0.9,
         italics = true,
         disable_italics_comments = false,
         ui_contrast = "low",
--- nvim_mauro/lua/plugins/UI/everforest.lua
+++ nvim_mauro/lua/plugins/UI/everforest.lua
@@ -1,13 +1,13 @@
 return {
-  "neanias/everforest-nvim",
-  lazy = false,
-  config = function()
-    require("everforest").setup({
-      background = "hard",
-      transparent_background_level = 0,
-      italics = true,
-      disable_italics_comments = false,
-      ui_contrast = "low",
-    })
-  end,
+	"neanias/everforest-nvim",
+	lazy = false,
+	config = function()
+		require("everforest").setup({
+			background = "hard",
+			transparent_background_level = 0.9,
+			italics = true,
+			disable_italics_comments = false,
+			ui_contrast = "low",
+		})
+	end,
 }
