657e357b50aa919b1d3a30f1a00c52d35fd9997f
mauromotion <hello@mauromotion.com>
2023-09-28 12:52:58 +0100

    added vimwiki search plugin for telescope

--- nvim_mauro/lua/plugins/navigation/telescope.lua
+++ nvim_mauro/lua/plugins/navigation/telescope.lua
@@ -5,11 +5,13 @@ return {
 	-- version = false,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		"nvim-tree/nvim-web-devicons",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"ElPiloto/telescope-vimwiki.nvim",
 	},
 	config = function()
 		require("telescope").load_extension("fzf")
+		require("telescope").load_extension("vimwiki")
 
 		-- Keymaps
 		local map = vim.keymap
@@ -18,6 +20,8 @@ return {
 		map.set("n", "<leader>fr", "<CMD>Telescope oldfiles<CR>", { desc = "Fuzzy [f]ind [r]ecent files" })
 		map.set("n", "<leader>fg", "<CMD>Telescope live_grep<CR>", { desc = "[F]ind strin[g] in cwd" })
 		map.set("n", "<leader>fs", "<CMD>Telescope grep_string<CR>", { desc = "[F]ind [s]tring under cursor in cwd" })
+		map.set("n", "<leader>fv", "<CMD>Telescope vimwiki<CR>", { desc = "[F]ind [V]imWiki pages" })
+		map.set("n", "<leader>fw", "<CMD>Telescope vimwiki live_grep<CR>", { desc = "Live Grep inside Vim[W]iki" })
 		map.set("n", "<leader>fb", function()
 			require("telescope.builtin").buffers({ sort_lastused = true })
 		end, { desc = "[F]ind open [B]uffers" })
