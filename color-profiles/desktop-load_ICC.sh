f105f159eea1f29fa0ab7cbbbea0f51f99c07787
mauromotion <hello@mauromotion.com>
2023-10-04 19:16:15 +0100

    added taskwiki script on polybar, fixed ICC files loading, reduced transparency on alacritty

--- color-profiles/desktop-load_ICC.sh
+++ color-profiles/desktop-load_ICC.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-/usr/bin/dispwin -d1 ./icc/Dell_S2721DGF.icm
+dispwin -d1 "$HOME/.dotfiles/color-profiles/icc/Dell_S2721DGF.icm"
 
 sleep 1
 
 /usr/bin/dispwin -d2 ./icc/Dell_U2713HM.icm
+dispwin -d2 "$HOME/.dotfiles/color-profiles/icc/Dell_U2713HM.icm"
