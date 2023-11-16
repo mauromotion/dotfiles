82b1b3b3a1bffcb40fdeb4608c8582585c260e25
mauromotion <hello@mauromotion.com>
2023-11-16 10:25:15 +0000

    added aerial plugin, added file's parent dir to lualine

--- nvim_mauro/lua/plugins/UI/lualine.lua
+++ nvim_mauro/lua/plugins/UI/lualine.lua
@@ -24,15 +24,15 @@ return {
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "filename" },
-				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_c = { { "filename", path = 4 } },
+				lualine_x = { "aerial", "encoding", "fileformat", "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
 			},
 			inactive_sections = {
 				lualine_a = {},
 				lualine_b = {},
-				lualine_c = { "filename" },
+				lualine_c = { { "filename", path = 4 } },
 				lualine_x = { "location" },
 				lualine_y = {},
 				lualine_z = {},
