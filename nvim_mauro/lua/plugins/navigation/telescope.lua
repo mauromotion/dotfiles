45e04b92dfdf04a4be2989f1c3e07553e80347eb
mauromotion <hello@mauromotion.com>
2023-11-24 15:58:20 +0000

    changed telescope shortcuts for buffers and find files

--- nvim_mauro/lua/plugins/navigation/telescope.lua
+++ nvim_mauro/lua/plugins/navigation/telescope.lua
@@ -16,13 +16,13 @@ return {
 		-- Keymaps
 		local map = vim.keymap
 
-		map.set("n", "<leader><space>", "<CMD>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
+		map.set("n", "<leader>f", "<CMD>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
 		map.set("n", "<leader>fr", "<CMD>Telescope oldfiles<CR>", { desc = "Fuzzy [f]ind [r]ecent files" })
 		map.set("n", "<leader>fg", "<CMD>Telescope live_grep<CR>", { desc = "[F]ind strin[g] in cwd" })
 		map.set("n", "<leader>fs", "<CMD>Telescope grep_string<CR>", { desc = "[F]ind [s]tring under cursor in cwd" })
 		map.set("n", "<leader>fv", "<CMD>Telescope vimwiki<CR>", { desc = "[F]ind [V]imWiki pages" })
 		map.set("n", "<leader>fw", "<CMD>Telescope vimwiki live_grep<CR>", { desc = "Live Grep inside Vim[W]iki" })
-		map.set("n", "<leader>fb", function()
+		map.set("n", "<leader><space>", function()
 			require("telescope.builtin").buffers({ sort_lastused = true })
 		end, { desc = "[F]ind open [B]uffers" })
 	end,
