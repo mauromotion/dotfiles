509152517bec2988f50fb2310af39b3ac77461aa
mauromotion <hello@mauromotion.com>
2023-11-24 12:10:54 +0000

    switched to JetBrainsMono in wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,10 +9,10 @@ end
 -- Configuration
 config.color_scheme_dirs = { "~/.config/wezterm/colors" }
 config.color_scheme = "nord"
-config.font = wezterm.font("FiraCode Nerd Font")
+config.font = wezterm.font("JetBrainsMono NF")
 config.font_size = 12
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
-config.window_background_opacity = 1
+config.window_background_opacity = 0.93
 
 return config
