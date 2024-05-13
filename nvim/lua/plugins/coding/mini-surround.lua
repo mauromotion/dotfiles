c8f06564925c31292512cccf7318f6ca4f3ce716
mauromotion <hello@mauromotion.com>
2024-05-13 17:25:42 +0100

    added marks.nvim plugin

--- nvim/lua/plugins/coding/mini-surround.lua
+++ nvim/lua/plugins/coding/mini-surround.lua
@@ -1,21 +1,20 @@
 return {
 	"echasnovski/mini.surround",
 	version = false,
-  keys = { "ma", "md", "mf", "mF", "mh", "mr", "mn" },
+	keys = { "ma", "md", "mf", "mF", "mh", "mr", "mn" },
 	config = function()
-		require("mini.surround").setup {
-      mappings = {
-        add = "ma", -- Add surrounding in Normal and Visual modes
-        delete = "md", -- Delete surrounding
-        find = "mf", -- Find surrounding (to the right)
-        find_left = "mF", -- Find surrounding (to the left)
-        highlight = "mh", -- Highlight surrounding
-        replace = "mr", -- Replace surrounding
-        update_n_lines = "mn", -- Update `n_lines`
-        suffix_last = "l", -- Suffix to search with "prev" method
-        suffix_next = "n", -- Suffix to search with "next" method
-      },
-    }
+		require("mini.surround").setup({
+			mappings = {
+				add = "gsa", -- Add surrounding in Normal and Visual modes
+				delete = "gsd", -- Delete surrounding
+				find = "gsf", -- Find surrounding (to the right)
+				find_left = "gsF", -- Find surrounding (to the left)
+				highlight = "gsh", -- Highlight surrounding
+				replace = "gsr", -- Replace surrounding
+				update_n_lines = "gsn", -- Update `n_lines`
+				suffix_last = "l", -- Suffix to search with "prev" method
+				suffix_next = "n", -- Suffix to search with "next" method
+			},
+		})
 	end,
 }
-
