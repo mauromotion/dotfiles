7698d6c2e2df48ed4e90fda96d231011d7b053f7
mauromotion <hello@mauromotion.com>
2024-05-31 15:29:22 +0100

    redirected vimwiki to the new index.md location

--- nvim/lua/plugins/utilities/vimwiki.lua
+++ nvim/lua/plugins/utilities/vimwiki.lua
@@ -6,7 +6,7 @@ return {
 		-- Default directory, syntax and file type, symbols for spaces, auto re-index tags db
 		vim.g.vimwiki_list = {
 			{
-				path = "~/Notes/VimWiki",
+				path = "~/Notes/VimWiki/notes",
 				syntax = "markdown",
 				ext = ".md",
 				links_space_char = "_",
