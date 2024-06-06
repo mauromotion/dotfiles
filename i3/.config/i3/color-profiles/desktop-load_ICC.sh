bb23be5718774be1dd1b0b7d600dafef3cb03aa7
mauromotion <hello@mauromotion.com>
2024-06-06 17:33:34 +0100

    fixed ICC profiles loading issue

--- i3/.config/i3/color-profiles/desktop-load_ICC.sh
+++ i3/.config/i3/color-profiles/desktop-load_ICC.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-dispwin -d1 "./icc/Dell_S2721DGF.icm"
+dispwin -d1 "$HOME/.dotfiles/i3/.config/i3/color-profiles/icc/Dell_S2721DGF.icm"
 
 sleep 1
 
 /usr/bin/dispwin -d2 ./icc/Dell_U2713HM.icm
-dispwin -d2 "./icc/Dell_U2713HM.icm"
+dispwin -d2 "$HOME/.dotfiles/i3/.config/i3/color-profiles/icc/Dell_U2713HM.icm"
