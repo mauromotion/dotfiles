904eeeefd1540cafde2026084bab4d575c1008a6
mauromotion <hello@mauromotion.com>
2024-06-13 14:31:15 +0100

    added 'del' alias to 'trash-put', removed 'tr' and 'trash' aliases. Added move to trash in oil.nvim

--- nvim/.config/nvim/lua/plugins/navigation/oil.lua
+++ nvim/.config/nvim/lua/plugins/navigation/oil.lua
@@ -1,10 +1,18 @@
 return {
 	"stevearc/oil.nvim",
-  event = "VimEnter",
+	event = "VimEnter",
 	config = function()
-		require("oil").setup()
-    -- Keybindings
-    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { noremap = true, silent = true, desc = "Open parent directory" }
-    )
+		require("oil").setup({
+			default_file_explorer = true,
+			delete_to_trash = true,
+			skip_confirm_for_simple_edits = true,
+			columns = {
+				"mtime",
+				"size",
+				"icon",
+			},
+		})
+		-- Keybindings
+		-- vim.keymap.set("n", "-", "<CMD>Oil<CR>", { noremap = true, silent = true, desc = "Open parent directory" })
 	end,
 }
