e27c65ac16fe446bc9a4bb693c0fb2ac632fbdaa
mauromotion <hello@mauromotion.com>
2024-06-14 09:54:14 +0100

    merged laptop and desktop dotfiles

---
+++ laptop/i3/.config/i3/color-profiles/laptop-load_ICC.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+dispwin "$HOME/.dotfiles/i3/.config/i3/color-profiles/icc/Gigabyte_Aero_15X.icm"
+
---
+++ laptop/polybar/.config/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
+
+# Launch Polybar, using default config location ~/.config/polybar/config.ini
+polybar laptop 2>&1 | tee -a /tmp/polybar.log &
+disown
+polybar mac 2>&1 | tee -a /tmp/polybar.log &
+disown
+
+echo "Polybar launched..."
