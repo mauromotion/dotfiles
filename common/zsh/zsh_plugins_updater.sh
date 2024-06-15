6a5103c4316367da1747df26b0df9879f27ea213
mauromotion <hello@mauromotion.com>
2024-06-15 23:58:00 +0100

    cleaning up old zsh files

--- common/zsh/zsh_plugins_updater.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "$HOME/.zsh/plugins/zsh-vi-mode/" && git pull
-cd "$HOME/.zsh/plugins/zsh-completions/" && git pull
-cd "$HOME/.zsh/plugins/zsh-autosuggestions/" && git pull
-cd "$HOME/.zsh/plugins/zsh-syntax-highlighting/" && git pull
-cd "$HOME/.zsh/plugins/fzf-zsh-plugin/" && git pull
-cd "$HOME/.zsh/plugins/pure/" && git pull
-cd "$HOME/.zsh/plugins/fzf-tab/" && git pull
