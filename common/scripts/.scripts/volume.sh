a9ddee530afb1096d8693762e7f2cae7e494c799
mauromotion <hello@mauromotion.com>
2024-07-09 17:08:16 +0100

    added muted to volume script

--- common/scripts/.scripts/volume.sh
+++ common/scripts/.scripts/volume.sh
@@ -2,6 +2,7 @@
 
 # Get the volume from wpctl
 vol="$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2}')"
+check_muted="$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $3}')"
 
 # Convert the volume from float to percentage
 percentage=$(printf "%.0f\n" $(echo "$vol * 100" | bc))
@@ -13,7 +14,10 @@ vol_int=$(echo $percentage | grep -oP '[0-9]+')
 icon=""
 
 # Show a different icon for each volume level
-if [ "$vol_int" -eq 0 ]; then
+if [ "$check_muted" == "[MUTED]" ]; then
+	icon="" # Mute
+	percentage="muted"
+elif [ "$vol_int" -eq 0 ]; then
 	icon="" # Mute
 elif [ "$vol_int" -gt 0 ] && [ "$vol_int" -le 20 ]; then
 	icon="" # Low volume
