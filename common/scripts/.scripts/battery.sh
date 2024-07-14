34a14dd050d2352f92465e34f0b55d7e767fdce7
mauromotion <hello@mauromotion.com>
2024-07-14 11:29:38 +0100

    battery script updated

--- common/scripts/.scripts/battery.sh
+++ common/scripts/.scripts/battery.sh
@@ -2,7 +2,34 @@
 
 batt=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep percentage)
 batt_int=$(echo $batt | grep -oP '[0-9]+')
+state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep state | awk '{print $2}')
 
 icon=""
 
-echo -e "  ${batt_int}%"
+if [[ "$state" == *"charging"* ]]; then
+	icon="󰂄"
+else
+	if [ "$batt_int" -eq 100 ]; then
+		icon="󰁹"
+	elif [ "$batt_int" -gt 90 ] && [ "$batt_int" -lt 100 ]; then
+		icon="󰂂"
+	elif [ "$batt_int" -gt 80 ] && [ "$batt_int" -lt 90 ]; then
+		icon="󰂁"
+	elif [ "$batt_int" -gt 70 ] && [ "$batt_int" -lt 80 ]; then
+		icon="󰂀"
+	elif [ "$batt_int" -gt 60 ] && [ "$batt_int" -lt 70 ]; then
+		icon="󰁿"
+	elif [ "$batt_int" -gt 50 ] && [ "$batt_int" -lt 60 ]; then
+		icon="󰁾"
+	elif [ "$batt_int" -gt 40 ] && [ "$batt_int" -lt 50 ]; then
+		icon="󰁽"
+	elif [ "$batt_int" -gt 30 ] && [ "$batt_int" -lt 40 ]; then
+		icon="󰁼"
+	elif [ "$batt_int" -gt 20 ] && [ "$batt_int" -lt 10 ]; then
+		icon="󰁻"
+	elif [ "$batt_int" -gt 10 ] && [ "$batt_int" -lt 0 ]; then
+		icon="󰁺"
+	fi
+fi
+
+echo -e "${icon} ${batt_int}%"
