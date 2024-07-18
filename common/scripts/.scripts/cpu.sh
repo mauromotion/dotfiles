24b5101801c04c8c7bec4606c265eb43922209c0
mauromotion <hello@mauromotion.com>
2024-07-19 00:35:58 +0100

    fixed cpu load script

--- common/scripts/.scripts/cpu.sh
+++ common/scripts/.scripts/cpu.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-cpu="$(top -bn1 | awk '/Cpu/ { print $2}')"
+# cpu="$(top -bn1 | awk '/Cpu/ { print $2}')"
+cpu="$(
+	top -bn1 | grep "Cpu(s)" |
+		sed "s/.*, *\([0-9.]*\)%* id.*/\1/" |
+		awk '{print 100 - $1}'
+)"
 echo -e " $cpu%"
