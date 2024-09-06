1ee508b21267fffbf8c9f3c48bd9fafcdfe6c4c1
mauromotion <hello@mauromotion.com>
2024-09-06 22:10:07 +0100

    fixed inverted resizing of clients in awesome

--- common/awesome/.config/awesome/bindings/global/global_colemak_dh.lua
+++ common/awesome/.config/awesome/bindings/global/global_colemak_dh.lua
@@ -176,10 +176,10 @@ awful.keyboard.append_global_keybindings({
 		awful.client.swap.byidx(-1)
 	end, { description = "swap with previous client by index", group = "client" }),
 	awful.key({ modkey }, "u", awful.client.urgent.jumpto, { description = "jump to urgent client", group = "client" }),
-	awful.key({ modkey }, "n", function()
+	awful.key({ modkey }, "o", function()
 		awful.tag.incmwfact(0.05)
 	end, { description = "increase master width factor", group = "layout" }),
-	awful.key({ modkey }, "o", function()
+	awful.key({ modkey }, "n", function()
 		awful.tag.incmwfact(-0.05)
 	end, { description = "decrease master width factor", group = "layout" }),
 	awful.key({ modkey, "Shift" }, "h", function()
