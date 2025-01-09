44afd729c1676fb806f95bf4d018bde029a7cc7d
mauromotion <hello@mauromotion.com>
2025-01-09 15:18:44 +0000

    added MarkdownPreviewToggle keybinding

--- common/nvim/.config/nvim/lua/plugins/utilities/markdown-preview.lua
+++ common/nvim/.config/nvim/lua/plugins/utilities/markdown-preview.lua
@@ -6,4 +6,16 @@ return {
 		vim.g.mkdp_filetypes = { "markdown", "vimwiki" }
 		vim.fn["mkdp#util#install"]()
 	end,
+	config = function()
+		local wk = require("which-key")
+
+		wk.add({
+			{
+				"<leader>cp",
+				"<CMD>MarkdownPreviewToggle<CR>",
+				ft = { "markdown", "vimwiki" },
+				desc = "Toggle MarkdownPreview",
+			},
+		})
+	end,
 }
