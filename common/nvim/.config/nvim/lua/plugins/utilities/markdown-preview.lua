d3a29d653574c29e0ac8cea41dfeee81b52c38c2
mauromotion <hello@mauromotion.com>
2025-01-09 15:10:00 +0000

    markdown-preview now can work with vimwiki ft

--- common/nvim/.config/nvim/lua/plugins/utilities/markdown-preview.lua
+++ common/nvim/.config/nvim/lua/plugins/utilities/markdown-preview.lua
@@ -3,6 +3,7 @@ return {
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 	ft = { "markdown" },
 	build = function()
+		vim.g.mkdp_filetypes = { "markdown", "vimwiki" }
 		vim.fn["mkdp#util#install"]()
 	end,
 }
