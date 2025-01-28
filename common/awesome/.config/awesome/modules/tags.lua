1d919fce0e3ec342c2b5d632e75c9e975990559e
mauromotion <hello@mauromotion.com>
2025-01-28 22:52:16 +0000

    awesomewm tags are now in their own module

---
+++ common/awesome/.config/awesome/modules/tags.lua
@@ -0,0 +1,168 @@
+----* Imports *----
+local beautiful = require("beautiful") -- Theme handling library
+local awful = require("awful") -- Standard awesome library
+
+----* Generate tags *----
+local function setup_tags(s)
+	local l = awful.layout.suit -- Just to save some typing: use an alias.
+
+	local tags = {
+		{
+			name = "home",
+			icon_unfocused = beautiful.tag_icon_home_unfocused,
+			icon_focused = beautiful.tag_icon_home_focused,
+		},
+		{
+			name = "mail",
+			icon_unfocused = beautiful.tag_icon_mail_unfocused,
+			icon_focused = beautiful.tag_icon_mail_focused,
+		},
+		{
+			name = "dev",
+			icon_unfocused = beautiful.tag_icon_dev_unfocused,
+			icon_focused = beautiful.tag_icon_dev_focused,
+		},
+		{
+			name = "chat",
+			icon_unfocused = beautiful.tag_icon_chat_unfocused,
+			icon_focused = beautiful.tag_icon_chat_focused,
+		},
+		{
+			name = "docs",
+			icon_unfocused = beautiful.tag_icon_docs_unfocused,
+			icon_focused = beautiful.tag_icon_docs_focused,
+		},
+		{
+			name = "media",
+			icon_unfocused = beautiful.tag_icon_media_unfocused,
+			icon_focused = beautiful.tag_icon_media_focused,
+		},
+		{
+			name = "games",
+			icon_unfocused = beautiful.tag_icon_games_unfocused,
+			icon_focused = beautiful.tag_icon_games_focused,
+		},
+		{
+			name = "edit",
+			icon_unfocused = beautiful.tag_icon_edit_unfocused,
+			icon_focused = beautiful.tag_icon_edit_focused,
+		},
+		{
+			name = "extra",
+			icon_unfocused = beautiful.tag_icon_extra_unfocused,
+			icon_focused = beautiful.tag_icon_extra_focused,
+		},
+	}
+
+	-- Define tags
+	awful.tag.add("home", {
+		icon = "",
+		icon_only = true,
+		layout = l.tile.left,
+		master_fill_policy = "expand",
+		gap_single_client = false,
+		gap = 4,
+		screen = s,
+		selected = true,
+	})
+
+	awful.tag.add("mail", {
+		icon = "",
+		icon_only = true,
+		layout = l.tile.left,
+		master_fill_policy = "expand",
+		gap_single_client = true,
+		gap = 18,
+		screen = s,
+	})
+
+	awful.tag.add("dev", {
+		icon = "",
+		icon_only = true,
+		layout = l.tile.left,
+		master_fill_policy = "expand",
+		-- gap_single_client = false,
+		-- gap = 0,
+		screen = s,
+	})
+
+	awful.tag.add("chat", {
+		icon = "",
+		icon_only = true,
+		layout = l.fair,
+		-- master_fill_policy = "expand",
+		-- gap_single_client = false,
+		gap = 4,
+		screen = s,
+	})
+
+	awful.tag.add("docs", {
+		icon = "",
+		icon_only = true,
+		layout = l.tile,
+		master_fill_policy = "expand",
+		gap_single_client = false,
+		gap = 6,
+		screen = s,
+	})
+
+	awful.tag.add("media", {
+		icon = "",
+		icon_only = true,
+		layout = l.tile.left,
+		master_fill_policy = "expand",
+		gap_single_client = false,
+		gap = 10,
+		screen = s,
+	})
+
+	awful.tag.add("games", {
+		icon = "",
+		icon_only = true,
+		layout = l.floating,
+		-- master_fill_policy = "master_width_factor",
+		-- gap_single_client = false,
+		-- gap = 0,
+		screen = s,
+	})
+
+	awful.tag.add("edit", {
+		icon = "",
+		icon_only = true,
+		layout = l.tile.left,
+		master_fill_policy = "expand",
+		-- gap_single_client = false,
+		-- gap = 0,
+		screen = s,
+	})
+
+	awful.tag.add("extra", {
+		icon = "",
+		icon_only = true,
+		layout = l.tile.top,
+		-- master_fill_policy = "master_width_factor",
+		gap_single_client = false,
+		gap = 6,
+		screen = s,
+	})
+
+	-- Loop through screens and tags
+	for sc = 1, screen.count() do
+		for i, tag in ipairs(awful.tag.gettags(sc)) do
+			local tag_info = tags[i]
+
+			-- Connect to the selected signal for each tag
+			tag:connect_signal("property::selected", function(t)
+				if t.selected then
+					-- Set the focused icon
+					t.icon = tag_info.icon_focused
+				else
+					-- Set the unfocused icon
+					t.icon = tag_info.icon_unfocused
+				end
+			end)
+		end
+	end
+end
+
+return setup_tags
--- common/awesome/.config/awesome/modules/toolbar.lua
+++ common/awesome/.config/awesome/modules/toolbar.lua
@@ -1,204 +1,21 @@
 ----* Imports *----
 local awful = require("awful") -- Standard awesome library
-local beautiful = require("beautiful") -- Theme handling library
 local wibox = require("wibox") -- Widget and layout library
 local vars = require("config.vars")
 local custom_funcs = require("utils.custom_functions")
+local setup_tags = require("modules.tags")
 
 ----* Toolbar *-----
-print("Toolbar is being set up!")
 screen.connect_signal("request::desktop_decoration", function(s)
-	print("Desktop decoration request received!")
-	-- Each screen has its own tag table.
-	-- local names = { " home ", " mail ", " dev ", " chat ", " docs ", " media ", " games ", " edit ", " xtra " }
-	local l = awful.layout.suit -- Just to save some typing: use an alias.
-	-- local layouts = { l.tile.left, l.max, l.tile, l.tile, l.tile, l.tile, l.floating, l.tile, l.tile.top }
-	-- awful.tag(names, s, layouts)
-	--
 	tag.connect_signal("request::default_layouts", function()
 		awful.layout.append_default_layouts(vars.layouts)
 	end)
 
-	local tags = {
-		{
-			name = "home",
-			icon_unfocused = beautiful.tag_icon_home_unfocused,
-			icon_focused = beautiful.tag_icon_home_focused,
-		},
-		{
-			name = "mail",
-			icon_unfocused = beautiful.tag_icon_mail_unfocused,
-			icon_focused = beautiful.tag_icon_mail_focused,
-		},
-		{
-			name = "dev",
-			icon_unfocused = beautiful.tag_icon_dev_unfocused,
-			icon_focused = beautiful.tag_icon_dev_focused,
-		},
-		{
-			name = "chat",
-			icon_unfocused = beautiful.tag_icon_chat_unfocused,
-			icon_focused = beautiful.tag_icon_chat_focused,
-		},
-		{
-			name = "docs",
-			icon_unfocused = beautiful.tag_icon_docs_unfocused,
-			icon_focused = beautiful.tag_icon_docs_focused,
-		},
-		{
-			name = "media",
-			icon_unfocused = beautiful.tag_icon_media_unfocused,
-			icon_focused = beautiful.tag_icon_media_focused,
-		},
-		{
-			name = "games",
-			icon_unfocused = beautiful.tag_icon_games_unfocused,
-			icon_focused = beautiful.tag_icon_games_focused,
-		},
-		{
-			name = "edit",
-			icon_unfocused = beautiful.tag_icon_edit_unfocused,
-			icon_focused = beautiful.tag_icon_edit_focused,
-		},
-		{
-			name = "extra",
-			icon_unfocused = beautiful.tag_icon_extra_unfocused,
-			icon_focused = beautiful.tag_icon_extra_focused,
-		},
-	}
-
-	-- Define tags
-	awful.tag.add("home", {
-		icon = "",
-		icon_only = true,
-		layout = l.tile.left,
-		master_fill_policy = "expand",
-		gap_single_client = false,
-		gap = 4,
-		screen = s,
-		selected = true,
-	})
-
-	awful.tag.add("mail", {
-		icon = "",
-		icon_only = true,
-		layout = l.tile.left,
-		master_fill_policy = "expand",
-		gap_single_client = true,
-		gap = 18,
-		screen = s,
-	})
-
-	awful.tag.add("dev", {
-		icon = "",
-		icon_only = true,
-		layout = l.tile.left,
-		master_fill_policy = "expand",
-		-- gap_single_client = false,
-		-- gap = 0,
-		screen = s,
-	})
-
-	awful.tag.add("chat", {
-		icon = "",
-		icon_only = true,
-		layout = l.fair,
-		-- master_fill_policy = "expand",
-		-- gap_single_client = false,
-		gap = 4,
-		screen = s,
-	})
-
-	awful.tag.add("docs", {
-		icon = "",
-		icon_only = true,
-		layout = l.tile,
-		master_fill_policy = "expand",
-		gap_single_client = false,
-		gap = 6,
-		screen = s,
-	})
-
-	awful.tag.add("media", {
-		icon = "",
-		icon_only = true,
-		layout = l.tile.left,
-		master_fill_policy = "expand",
-		gap_single_client = false,
-		gap = 10,
-		screen = s,
-	})
-
-	awful.tag.add("games", {
-		icon = "",
-		icon_only = true,
-		layout = l.floating,
-		-- master_fill_policy = "master_width_factor",
-		-- gap_single_client = false,
-		-- gap = 0,
-		screen = s,
-	})
-
-	awful.tag.add("edit", {
-		icon = "",
-		icon_only = true,
-		layout = l.tile.left,
-		master_fill_policy = "expand",
-		-- gap_single_client = false,
-		-- gap = 0,
-		screen = s,
-	})
-
-	awful.tag.add("extra", {
-		icon = "",
-		icon_only = true,
-		layout = l.tile.top,
-		-- master_fill_policy = "master_width_factor",
-		gap_single_client = false,
-		gap = 6,
-		screen = s,
-	})
-
-	-- Loop through screens and tags
-	for sc = 1, screen.count() do
-		for i, tag in ipairs(awful.tag.gettags(sc)) do
-			local tag_info = tags[i]
-
-			-- Connect to the selected signal for each tag
-			tag:connect_signal("property::selected", function(t)
-				if t.selected then
-					-- Set the focused icon
-					t.icon = tag_info.icon_focused
-				else
-					-- Set the unfocused icon
-					t.icon = tag_info.icon_unfocused
-				end
-			end)
-		end
-	end
+	-- Create tags
+	setup_tags(s)
 
 	-- Create a promptbox for each screen
 	s.mypromptbox = awful.widget.prompt()
-
-	-- Create an imagebox widget which will contain an icon indicating which layout we're using.
-	-- We need one layoutbox per screen.
-	s.mylayoutbox = awful.widget.layoutbox({
-		screen = s,
-		buttons = {
-			awful.button({}, 1, function()
-				awful.layout.inc(1)
-			end),
-			awful.button({}, 3, function()
-				awful.layout.inc(-1)
-			end),
-			awful.button({}, 4, function()
-				awful.layout.inc(-1)
-			end),
-			awful.button({}, 5, function()
-				awful.layout.inc(1)
-			end),
-		},
-	})
 
 	-- Create a taglist widget
 	s.mytaglist = awful.widget.taglist({
@@ -276,15 +93,6 @@ screen.connect_signal("request::desktop_decoration", function(s)
 	-- Customize the systray
 	local mysystray = wibox.widget.systray()
 	mysystray:set_base_size(20)
-
-	-- Move the systray on secondary monitor for desktop PC, and keep it on primary for laptop
-	-- get_hostname(function(hostname)
-	-- 	if hostname == "eva-01" then
-	-- 		mysystray:set_screen(screen[2])
-	-- 	else
-	-- 		mysystray:set_screen("primary")
-	-- 	end
-	-- end)
 
 	-- Move the systray on the secondary monitor, if secondary monitor is present
 	if screen[2] then
