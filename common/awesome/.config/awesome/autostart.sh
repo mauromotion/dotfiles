68bd04a565a54ca6ea382b786ef6fa6d4f5a9f44
mauromotion <hello@mauromotion.com>
2025-02-08 20:10:17 +0000

    added blur to betterlockscreen command

--- common/awesome/.config/awesome/autostart.sh
+++ common/awesome/.config/awesome/autostart.sh
@@ -34,5 +34,5 @@ nextcloud --background &
 openrgb --startminimized &
 xset b 100 &
 xbindkeys &
-xautolock -time 10 -locker "betterlockscreen -l dim" -detectsleep &
+xautolock -time 10 -locker "betterlockscreen -l dimblur" -detectsleep &
 ~/.icc_color_profiles/load_icc_color_profiles.sh &
