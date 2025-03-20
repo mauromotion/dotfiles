1afd26726f00c7f1088bac778fcdb1044b49f7bd
mauromotion <hello@mauromotion.com>
2025-03-20 22:56:49 +0000

    excluded "nofile" from nvim-highlight-colors

--- common/nvim/.config/nvim/lua/plugins/utilities/nvim-highlight-colors.lua
+++ common/nvim/.config/nvim/lua/plugins/utilities/nvim-highlight-colors.lua
@@ -1,9 +1,12 @@
 return {
 	"brenoprata10/nvim-highlight-colors",
+	event = "VeryLazy",
 	config = function()
 		require("nvim-highlight-colors").setup({
 			render = "background",
 			enable_tailwind = true,
+			exclude_filetypes = { "nofile" },
+			exclude_buftypes = { "nofile" },
 		})
 	end,
 }
