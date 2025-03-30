37633a805f9c983c72436f5af807a3ec24f30efe
mauromotion <hello@mauromotion.com>
2025-03-31 00:38:56 +0100

    added style for vim-modes

--- common/fish/.config/fish/config.fish
+++ common/fish/.config/fish/config.fish
@@ -5,55 +5,6 @@
 ## * .dotfiles * --
 ## Fish terminal -- config.fish --------------
 
-## Aliases
-alias .. "cd .."
-alias cd "z"
-alias cl "clear"
-alias colemak "setxkbmap gb -variant colemak_dh"
-alias cp "cp -i"
-alias del "trash-put -i"
-alias df "df -H"
-alias diary "nvim +VimwikiIndex +VimwikiDiaryIndex"
-alias etree "eza -T --color=always --icons | less -R"
-alias fastfetch "fastfetch -c examples/10.jsonc"
-abbr --add ga git add
-abbr --add gaa git add --all
-abbr --add gcam git commit --all --message
-abbr --add gP git push
-abbr --add gp git pull
-abbr --add gst git status
-abbr --add gsw git switch
-alias glog "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset' --date=short"
-alias grep "grep --color=auto"
-alias icat "kitty +kitten icat"
-alias in "tw add +in"
-alias lg "lazygit"
-alias ll "eza -l --git --icons -h"
-alias lla "eza -l --git --icons -h -a"
-alias load-ICC "sh ~/.icc_color_profiles/*.sh"
-alias ls "eza --icons"
-alias lsa "eza --icons -a"
-alias mirrors-update "sudo reflector -c 'United Kingdom' -a 6 --sort rate --save /etc/pacman.d/mirrorlist"
-alias mv "mv -i"
-alias ncdu "ncdu --color dark"
-alias next "tw +next"
-alias nv "nvim"
-alias pac-autoremove "sudo pacman -Rcs $(pacman -Qdtq)"
-alias polybar-restart "sh ~/.config/polybar/launch*"
-alias py "python3"
-alias qwerty "setxkbmap gb"
-alias rm "rm -i"
-alias rnd "tw +rnd"
-abbr --add src source ~/.config/fish/config.fish
-alias tw "task"
-alias v "nvim"
-alias vst "mullvad status"
-alias vup "mullvad connect"
-alias vdw "mullvad disconnect"
-alias wiki "nvim -c VimwikiIndex"
-alias y "yazi"
-alias yt "ytfzf"
-
 # ## Greeting message
 # function fish_greeting
 #     echo --------------------------------
@@ -66,14 +17,61 @@ alias yt "ytfzf"
 #     echo
 # end
 
-# note: this erases the default fish_mode_prompt, which adds a vi mode
-# indicator to the left of your prompt
-function fish_mode_prompt
-end
+## Set default editor
+set -gx EDITOR nvim 
+
 
 # turn on vi mode when the shell starts
-# fish_vi_mode
 fish_vi_key_bindings
+
+## Aliases & abbreviations
+abbr --add ga git add
+abbr --add gaa git add --all
+abbr --add gcam git commit --all --message
+abbr --add gP git push
+abbr --add gp git pull
+abbr --add gst git status
+abbr --add gsw git switch
+abbr --add refl sudo reflector -c \'United Kingdom\' -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+abbr --add src source ~/.config/fish/config.fish
+abbr --add vst mullvad status
+abbr --add vup mullvad connect
+abbr --add vdw mullvad disconnect
+alias .. "cd .."
+alias cd "z"
+alias cl "clear"
+alias colemak "setxkbmap gb -variant colemak_dh"
+alias cp "cp -i"
+alias del "trash-put -i"
+alias df "df -H"
+alias diary "nvim +VimwikiIndex +VimwikiDiaryIndex"
+alias etree "eza -T --color=always --icons | less -R"
+alias fastfetch "fastfetch -c examples/10.jsonc"
+alias glog "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset' --date=short"
+alias grep "grep --color=auto"
+alias icat "kitty +kitten icat"
+alias in "tw add +in"
+alias lg "lazygit"
+alias ll "eza -l --git --icons -h"
+alias lla "eza -l --git --icons -h -a"
+alias load-ICC "sh ~/.icc_color_profiles/*.sh"
+alias ls "eza --icons"
+alias lsa "eza --icons -a"
+alias mv "mv -i"
+alias ncdu "ncdu --color dark"
+alias next "tw +next"
+alias nv "nvim"
+alias pac-autoremove "sudo pacman -Rcs $(pacman -Qdtq)"
+alias polybar-restart "sh ~/.config/polybar/launch*"
+alias py "python3"
+alias qwerty "setxkbmap gb"
+alias rm "rm -i"
+alias rnd "tw +rnd"
+alias tw "task"
+alias v "nvim"
+alias wiki "nvim -c VimwikiIndex"
+alias y "yazi"
+alias yt "ytfzf"
 
 ## Poimandres Color Palette 
 set -l foreground e4f0fb
@@ -139,6 +137,28 @@ set -gx FZF_DEFAULT_OPTS '
   --pointer="◆"
   --separator="─"
   --scrollbar="│"'
+
+# Style the vim-mode indicator
+function fish_mode_prompt
+switch $fish_bind_mode
+    case default
+      set_color --bold $yellow
+      echo 'N '
+    case insert
+      set_color --bold $green
+      echo ''
+    case replace_one
+      set_color --bold $green
+      echo 'R '
+    case visual
+      set_color --bold $yellow
+      echo 'V '
+    case '*'
+      set_color --bold $red
+      echo '? '
+  end
+  set_color $foreground
+end
 
 ## Zoxide
 zoxide init fish | source
