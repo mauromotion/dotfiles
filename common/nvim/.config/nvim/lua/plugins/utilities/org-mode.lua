b1a0f7d6df68c11fc798af51c09d599215ca8d52
mauromotion <hello@mauromotion.com>
2025-04-17 15:30:15 +0100

    strop tracking fish_variables

--- common/nvim/.config/nvim/lua/plugins/utilities/org-mode.lua
+++ common/nvim/.config/nvim/lua/plugins/utilities/org-mode.lua
@@ -3,16 +3,18 @@ return {
 	event = "VeryLazy",
 	config = function()
 		require("orgmode").setup({
-			org_agenda_files = "~/Notes/orgfiles/**/*",
-			org_default_notes_file = "~/Notes/orgfiles/refile.org",
+			org_agenda_files = "~/Nextcloud/03_Resources/Notes/orgfiles/**/*",
+			org_default_notes_file = "~/Nextcloud/03_Resources/Notes/orgfiles/refile.org",
 			org_todo_keywords = {
 				"TODO(t)",
 				"DOING(p)",
 				"LATER(l)",
 				"WAIT(w)",
+				"HOLD(h)",
 				"IDEA(i)",
 				"|",
 				"DONE(d)",
+				"KILLED(k)",
 				"CANCELLED(c)",
 			},
 		})
