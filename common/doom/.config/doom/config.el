9c683a24b7128b2bbc3986e4966db97d73e309ba
mauromotion <hello@mauromotion.com>
2025-04-26 15:11:49 +0100

    (alacritty) added adwait and commit fonts

--- common/doom/.config/doom/config.el
+++ common/doom/.config/doom/config.el
@@ -32,8 +32,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'modus-operandi-tinted)
-(setq doom-font "Adwaita Mono")
+(setq doom-theme 'doom-zenburn)
+(setq doom-font "Iosevka Custom Extended")
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
--- common/nvim/.config/nvim/lua/plugins/UI/colorschemes.lua
+++ common/nvim/.config/nvim/lua/plugins/UI/colorschemes.lua
@@ -86,6 +86,7 @@ return {
 		lazy = false,
 		priority = 1000,
 		config = function()
+			vim.g.zenbones_lightness = "dim"
 			-- vim.g.zenwritten_darken_comments = 45
 			-- vim.g.zenwritten_darkness = "stark"
 			vim.cmd.colorscheme("zenbones")
