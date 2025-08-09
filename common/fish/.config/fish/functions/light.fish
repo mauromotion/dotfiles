432171a624b97a47156ae4b1ce67343ef44db35e
mauromotion <hello@mauromotion.com>
2025-08-09 13:02:42 +0100

    (emacs) added support for astro files, added emmet support

--- common/fish/.config/fish/functions/light.fish
+++ common/fish/.config/fish/functions/light.fish
@@ -5,6 +5,6 @@ function light -d "Set light theme"
   kitty @ set-colors -a "~/.local/share/nvim/lazy/zenbones.nvim/extras/kitty/zenbones_light.conf"
 
   # Alacritty
-  cp -f ~/.dotfiles/common/alacritty/.config/alacritty/themes/zenbones-light-alacritty-theme.toml \
+  cp -f ~/.dotfiles/common/alacritty/.config/alacritty/themes/zenbones-light-dim-alacritty-theme.toml \
      ~/.dotfiles/common/alacritty/.config/alacritty/themes/current-theme.toml
 end
